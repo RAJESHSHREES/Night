99cca41457195ba20191618947efb1bcd1be3524
0ashishale <ashishale42@gmail.com>
2022-05-25 14:45:23 +0545

    some css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,34 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Start Laravel. Jay Samboo</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Club</title>
+  <link rel="stylesheet" href = "/css/navbar.css" >
 </head>
 <body>
-    <div>
-        <h1>This is ashish ale magar</h1>
+   <div class="banner">
+      <div class="nav-bar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About US</a></li>
+          <li><a href="#">Servises</a></li>
+          <li><a href="#">Gallery</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">Reservation</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+    <div class="content">
+      <div class="section">
+        <p>ENJOY THE MUSIC</p>
+        <h1>MAKE YOUR NIGHT</h1>
+      </div>
+      <div class="info">
+        <a href="#">More Info</a>
+      </div>
+      </div>
     </div>
 </body>
 </html>
